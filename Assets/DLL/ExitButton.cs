ea05af2b4b8ac186bc02f5470bc322ecb5f73f89
nexylmao <vuletic.nenad@jjzmaj.edu.rs>
2018-10-05 12:39:28 +0200

    Exit button

---
+++ Assets/DLL/ExitButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour {
+
+	void Start () {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(OnClick);
+	}
+
+	void OnClick()
+	{
+		Application.Quit();
+	}
+}
